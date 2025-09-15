1cf7960946a69ff54e54c172a0897569117fbafc
peksmart <qcjxberin@outlook.com>
2025-09-15 17:31:15 +0800

    Add files via upload

---
+++ pkg/rustwasmdemo_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const add: (a: number, b: number) => number;
+export const multiply: (a: number, b: number) => number;
+export const greet: (a: number, b: number) => [number, number];
+export const factorial: (a: number) => number;
+export const is_prime: (a: number) => number;
+export const main: () => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
